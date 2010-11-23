02a1caf3dd089bea4d3a21325181b6aa32b04d2a
V L <noxxxone@mailhost.assembla.com>
2010-11-23 18:40:09 +0200

    Implemented adding OR filter
    TODO replace OR with delete filter link

--- filters.js
+++ filters.js
@@ -4,6 +4,8 @@ var Filters = {
 
     // key = type, value => [[id1, value1], [id2, value2]]
     op_enums: {},
+    group_idx : 0,
+    filter_idx: 1,
 
     get_field: function(field_name) {
         return $.grep(this.fields, function(el) { return el["name"] == field_name })[0]
@@ -29,17 +31,15 @@ var Filters = {
     },
 
     activate_filter_op: function(select) {
-        var idx = select.id.indexOf('_');
-
-        var prefix = select.id.substring(0, idx)
+        var prefix = this.el_prefix(select.id);
         var id_op = "#" + prefix + "_op"
         var op_select = $(id_op)[0];
 
         // if nothing is selected disable op && value
         if (select.selectedIndex == 0) {
             op_select.disabled = true;
-            $("#" + prefix + "_value").disabled = true;
-            $("#" +prefix + "_or_c").html("");
+            $("#" + prefix + "_value")[0].disabled = true;
+            $("#" + prefix + "_or_c").html("");
             return;
         }
 
@@ -61,8 +61,44 @@ var Filters = {
 
         $("#" + prefix + "_value_c").html(html);
         // Add OR button
-        $("#" +prefix + "_or_c").html("<input type='button' id='" + prefix + "_or_bt' value='OR' />");
-        $("#" + prefix + "_or_bt").click(function(evt){ alert(evt.target.id) });
+        $("#" + prefix + "_or_c").html("<input type='button' id='" + prefix + "_or_bt' value='OR' />");
+        $("#" + prefix + "_or_bt").click(function(evt){ Filters.add_or_line(evt.target.id) });
+    },
+
+    el_prefix: function(el_id) {
+        return el_id.substring(0, el_id.indexOf('_'));
+    },
+
+    add_or_line: function(bt_or_id) {
+        var prefix = this.el_prefix(bt_or_id);
+        this.filter_idx += 1;
+        // get group id
+        var group_id = $("#" + prefix + "_group")[0].value;
+        var html = '<div id="filter' + this.filter_idx + '">';
+        // add field select
+        html += "<select id='filters" + this.filter_idx + "_name' name='filters" + this.filter_idx + "[name]' onchange=\"Filters.activate_filter_op(this);\"></select>\n";
+        // add group hidden field
+        html += '<input type="hidden" id="filters'+this.filter_idx+'_group" name="filters'+this.filter_idx+'[group]" value="' + group_id + '" />';
+        // add op select disabled
+        html += "<select disabled=\"disabled\" id='filters" + this.filter_idx + "_op' name='filters" + this.filter_idx + "[op]'></select>\n";
+        // add value input disabled
+        html += '<span id="filters' + this.filter_idx +'_value_c">';
+        html += '<input disabled="disabled" type="text" id="filters' + this.filter_idx + '_value" name="filters' + this.filter_idx + '[value]">';
+        html += "</span>";
+        html += '<span id="filters' + this.filter_idx + '_or_c"></span>';
+        html += "</div>";
+        $('#fgroup' + group_id).append(html);
+
+        // TODO replace OR with delete link
+
+        // add fields to select
+        field_select = $("#filters" + this.filter_idx + "_name")[0];
+        field_select.add(new Option("",""));
+        $.map(this.fields, function(el) {field_select.add(new Option(el["caption"], el["name"]))});
+    },
+
+    render: function() {
+
     },
 
     init_filters: function () {
